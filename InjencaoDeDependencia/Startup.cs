918acb273277c4db60bb2c355f07a5ea16beb8a4
jrguedelha <jrguedelha@gmail.com>
2020-07-01 14:27:15 -0300

    Configuracao de Startup.cs

--- InjencaoDeDependencia/Startup.cs
+++ InjencaoDeDependencia/Startup.cs
@@ -1,26 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InjecaoDeDependencia.DI.Classes;
+using InjecaoDeDependencia.DI.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using InjecaoDeDependencia.DI.Classes;
-using InjecaoDeDependencia.DI.Interfaces;
 
 namespace InjencaoDeDependencia
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Registro das dependencias
+
             services.AddTransient<IMotoService>(DI => new MotoService("Jose", "XYZ-0000"));
             services.AddTransient<ICarroService>(DI => new CarroService("Joao"));
             services.AddTransient<ICaminhaoService>(DI => new CaminhaoService("José", "C"));
+
+            #endregion
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,15 +42,22 @@ namespace InjencaoDeDependencia
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "/");
             });
         }
     }
